5f8d9f6aa01c0993d146aca76a17d01a712f0183
Paul <96999089+GlassesAreForPseudoIntellegencia@users.noreply.github.com>
2022-01-08 15:35:06 -0800

    Not quite finalized autonomous

--- Roo.java
+++ Roo.java
@@ -1,5 +1,5 @@
 
-//Imports to get everything we need
+
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -8,12 +8,11 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-//labels code as autonomous
 @Autonomous(name="Roo", group="Linear Opmode")
 
 
 public class Roo extends LinearOpMode {
-//declaring the members of the code
+
     private ElapsedTime     runtime = new ElapsedTime();
     private DcMotor leftDrive = null;
     private DcMotor rightDrive = null;
@@ -24,13 +23,13 @@ public class Roo extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-        //tells the robot what is what and where it is
+
         leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
         rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
         leftDriver  = hardwareMap.get(DcMotor.class, "left_driver");
         rightDriver = hardwareMap.get(DcMotor.class, "right_driver");
         ernie_ducky = hardwareMap.get(DcMotor.class, "ernies_ducky");
-        //to make sure everything is going the same direction
+        
         leftDrive.setDirection(DcMotor.Direction.REVERSE);
         rightDrive.setDirection(DcMotor.Direction.FORWARD); 
         leftDriver.setDirection(DcMotor.Direction.REVERSE);
@@ -44,23 +43,24 @@ public class Roo extends LinearOpMode {
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
 
-        //tells the robot to go 1/4 full power backwards for 1/4 of a second
         leftDrive.setPower(-0.25);
         rightDrive.setPower(-0.25);
         leftDriver.setPower(-0.25);
         rightDriver.setPower(-0.25);        
-        while ((opModeIsActive()) && (runtime.seconds() < 0.22)) { //Without an encoder we have to rely on crude time measurements.
+        while ((opModeIsActive()) && (runtime.seconds() < 1.0)) {
             telemetry.addData("Status", "Runtime", runtime.seconds());
             telemetry.update();
         }
-        //tells the carousel to spin
+        
+        
+        
         ernie_ducky.setPower(1);
         runtime.reset();
-        while ((opModeIsActive()) && (runtime.seconds() < 2.0)) { //MAKE SURE TO TEST
+        while ((opModeIsActive()) && (runtime.seconds() < 10.0)) {
             telemetry.addData("Status", "Runtime", runtime.seconds());
             telemetry.update();
         }
-        //tells the robot steam ahead full power for 2 seconds
+        
         leftDrive.setPower(1);
         rightDrive.setPower(1);
         leftDriver.setPower(1);
@@ -69,12 +69,8 @@ public class Roo extends LinearOpMode {
         while (opModeIsActive() && (runtime.seconds() < 2.0)) {
             telemetry.addData("Status", "Runtime", runtime.seconds());
             telemetry.update();
+            
         }
-        //tells the robot to stop
-        leftDrive.setPower(0);
-        rightDrive.setPower(0);
-        leftDriver.setPower(0);
-        rightDriver.setPower(0);
     }
 }
 
